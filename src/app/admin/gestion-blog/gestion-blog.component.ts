e314ddeacc2975b7af4713f1df9c744d2b56b946
thiernombdiallo <bobo12diallo12@gmail.com>
2023-12-12 17:12:14 +0000

    deuxieme commit

--- src/app/admin/gestion-blog/gestion-blog.component.ts
+++ src/app/admin/gestion-blog/gestion-blog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 import { Article } from 'src/app/Models/article.model';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from 'src/app/services/article.service';
@@ -10,6 +10,12 @@ import Swal from 'sweetalert2';
   styleUrls: ['./gestion-blog.component.css']
 })
 export class GestionBlogComponent {
+
+  constructor(private articleService: ArticleService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.loadArticles();
+  }
 
   // attributs
   titreArticle: string = "";
@@ -26,20 +32,71 @@ export class GestionBlogComponent {
   // Tableau articles
   articles: Article[] = [];
 
-  constructor(private articleService: ArticleService, private route: ActivatedRoute) {}
+  // Attribut pour la pagination
+  articlesParPage = 10; // Nombre d'articles par page
+  pageActuelle = 1; // Page actuelle
 
-  ngOnInit(): void {
-    this.loadArticles();
+  // Attribut pour faire les recherche
+  searchArticle = '';
+  itemSearchs: any;
+
+  // Propriétés de pagination
+  itemsPerPage: number = 6;
+  currentPage: number = 1;
+
+  
+  //methode qui permet de faire la recherche
+  articleFound() {
+    if (this.searchArticle.trim() === '') {
+      this.itemSearchs = null; 
+    } else {
+      this.itemSearchs = this.articles.filter(
+        (item: Article) => item.titre.toLowerCase().includes(this.searchArticle.toLowerCase())
+      );
+    }
+    this.currentPage = 1;
   }
+
+  //methode pour gerer la pagination
+  get paginatedArticles(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.itemSearchs ? this.itemSearchs.slice(startIndex, endIndex) : this.articles.slice(startIndex, endIndex);
+  }
+
+  getPageForArticle(article: any): number {
+    const articleIndex = this.itemSearchs ? this.itemSearchs.indexOf(article) : this.articles.indexOf(article);
+    return Math.ceil((articleIndex + 1) / this.itemsPerPage);
+  }
+  
+  
+  // methodes qui gerent les page pour la pagination
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.getTotalPages()) {
+      this.currentPage = page;
+    }
+  }
+
+  getTotalPages(): number {
+    return Math.ceil((this.itemSearchs ? this.itemSearchs.length : this.articles.length) / this.itemsPerPage);
+  }
+
+  getPages(): number[] {
+    return Array.from({ length: this.getTotalPages() }, (_, i) => i + 1);
+  }
+
 
 
   // Récupération des articles 
   loadArticles() {
-      this.articleService.getArticles().subscribe((data) => {
-      this.articles = data;
+      this.articleService.getArticles().subscribe((reponse : any) => {
+        console.log("LISTE DATAS");
+        console.log(reponse);
+      this.articles = reponse.data;
+
     });
   }
-
 
   // Méthode pour ajouter un article
   addArticle() {
@@ -59,13 +116,21 @@ export class GestionBlogComponent {
       };
 
       this.articleService.addArticle(newArticle).subscribe(() => {
-
-        this.articles.unshift(newArticle);
-
+        // console.log('ok');
+        // this.articles.unshift(newArticle);
+        document.getElementById("close-modal")?.click()
         this.alertMessage("success","Bravo!","Article ajouté avec succés");
         this.loadArticles();
+        
+      // Réinitialiser les champs après l'ajout
+      this.titreArticle = "";
+      this.articlePhoto = "";
+      this.descriptionArticle = "";
+
       });
+      
     }
+    
 
   }
 
